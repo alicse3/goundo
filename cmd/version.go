f7ce7111d42c46024c85b7d2b8cbbb6abc557e8a
Ali <alicse3@gmail.com>
2024-08-25 15:47:12 +0530

    feat: simplify command handling

--- cmd/version.go
+++ cmd/version.go
@@ -1,33 +1,10 @@
 package cmd
 
-import (
-	"flag"
-	"os"
-)
+import "fmt"
 
+const latestVersion = "v1.0.0"
+
+// versionHandler prints the app's latest version
 func versionHandler() {
-	// Create a new flag set
-	flagSet := flag.NewFlagSet("version", flag.ExitOnError)
-
-	// Define custom usage function
-	flagSet.Usage = func() {
-		println("Usage: goundo version [flags]")
-		println("Flags:")
-		flagSet.PrintDefaults()
-	}
-
-	// Define the help flag
-	helpFlg := flagSet.Bool("h", false, "Display help information")
-
-	// Parse the command-line arguments
-	if err := flagSet.Parse(os.Args[2:]); err != nil {
-		panic(err)
-	}
-
-	// Check if the help flag is set
-	if *helpFlg {
-		flagSet.Usage()
-	} else {
-		println("1.0.0")
-	}
+	fmt.Println(latestVersion)
 }
